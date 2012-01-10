08fe720f531d06ab830fe51459497c8d546742bd
Fabio Benedetti <f.benedetti@stetel.com>
2012-01-10 17:25:53 +0100

    add test

---
+++ TabbyLauncher/src/org/tabbylauncher/test.java
@@ -0,0 +1,12 @@
+package org.tabbylauncher;
+
+import android.content.Context;
+
+public class test extends Rotor {
+
+	public test(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+}
